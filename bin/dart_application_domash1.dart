fbc55a3758371c7e04e816e519efe1f62a63cd6c
LordMaks2002 <124434358+LordMaks2002@users.noreply.github.com>
2024-10-15 21:23:55 +0300

    Task complete

--- bin/dart_application_domash1.dart
+++ bin/dart_application_domash1.dart
@@ -1,4 +1,19 @@
-import 'package:dart_application_domash1/dart_application_domash1.dart'
-    as dart_application_domash1;
+void main() {
+  int num = 17; //це зміна яку можна змінювати
+  if (isSimple(num) == true) {
+    /*провіряє чи isSimple є істина*/
+    print('$num - просте число');
+  } else {
+    print('$num - не просте число');
+  }
+}
 
-void main() {}
+bool isSimple(int value) {
+  if (value <= 1) return false; /*якщо воно менче за 1*/
+  for (int i = 2; i <= value ~/ 2; i++)
+  /*воно ділить без остатка і провіряє кожне число*/
+  {
+    if (value % i == 0) return false; /*провіряє залишок*/
+  }
+  return true;
+}
